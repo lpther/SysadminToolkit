cf242d0fe0c5a939fe7ed4e6c19461e0a4b26c1d
lpther <lpther@gmail.com>
2013-04-21 12:50:09 +0000

    * Added pipy template

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distribute_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup
+
+
+setup(
+    name='SysadminToolkit',
+    version='0.1.0',
+    author='Louis-Philippe Theriault',
+    author_email='lpther@gmail.com',
+    packages=['SysadminToolkit'],
+    url='',
+    license='See LICENSE.txt',
+    description='',
+    long_description=open('README.txt').read(),
+)

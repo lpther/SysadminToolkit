748920354abeca68c8f23c3fd014e0b57c94591d
Louis-Philippe Theriault <lpther@gmail.com>
2013-04-26 21:33:34 +0000

    * Added a few verbosity arguments * Populated default config * Logging is now handled cleanly

--- src/sysadmintoolkit/__main__.py
+++ src/sysadmintoolkit/__main__.py
@@ -3,8 +3,6 @@ import __init__ as appinfo
 import ConfigParser
 import argparse
 import signal
-import logging
-import sys
 import sysadmintoolkit
 
 
@@ -19,8 +17,14 @@ if __name__ == '__main__':
                            help='Main configuration file', type=str, action='store')
     argparser.add_argument('cmd', help='Execute this command in the toolkit and exit', type=str, \
                            nargs='*')
+    argparser.add_argument('--disable-shell', default=False, \
+                           help='All user controlled shell interactions ("!","|") are disabled', action='store_true')
+    argparser.add_argument('-q', '--quiet', default=False, \
+                           help='Turn all possible debugging to off, only stdout from commands are displayed', action='store_true')
     argparser.add_argument('-d', '--debug', default=False, \
                            help='Turn all possible debugging to on', action='store_true')
+    argparser.add_argument('-v', '--verbose', default=False, \
+                           help='Increase logging level to INFO', action='store_true')
     argparser.add_argument('-V', '--version', action='version', \
                            version='%(prog)s ' + '%s' % appinfo.__version__)
 
@@ -33,10 +37,25 @@ if __name__ == '__main__':
 
     # Defaults
     defaults = {'enable-plugin': 'yes',
-                'log-destination': 'stdout',
+                'log-destination': 'console',
                 'log-level': 'warning',
                 'name-resolution': 'yes',
                 }
+
+    # Fill the config defaults with arguments
+    override_logging_dest = None
+    override_logging_level = None
+
+    if args.quiet:
+        override_logging_dest = 'null'
+
+    if args.verbose:
+        override_logging_level = 'info'
+        override_logging_dest = 'console'
+
+    if args.debug:
+        override_logging_level = 'debug'
+        override_logging_dest = 'console'
 
     # Verify and load the configuration file
     try:
@@ -51,33 +70,45 @@ if __name__ == '__main__':
     except ConfigParser.Error as e:
         argparser.error('Configuration file could not be parsed:\n\n%s' % sysadmintoolkit.utils.indent_text(str(e)))
 
+    # Make sure the commandprompt plugin will be loaded
     if 'commandprompt' not in config.sections():
         config.add_section('commandprompt')
+
+    if not config.has_option('commandprompt', 'plugin-dir'):
+        config.set('commandprompt', 'plugin-dir', '/etc/sysadmin-toolkit/plugin.d/')
+
+    if not config.has_option('commandprompt', 'scripts-dir'):
+        config.set('commandprompt', 'script-dir', '/etc/sysadmin-toolkit/scripts.d/')
+
+    # Populate config with arguments
+    sysadmintoolkit.utils.set_config_logging(config, override_logging_dest, override_logging_level)
 
     if args.debug:
         sysadmintoolkit.utils.print_config_contents(config)
 
-    # ---- Initialize logging
-
-    ConsoleHandler = logging.StreamHandler(sys.stdout)
-    ConsoleHandler.setFormatter(logging.Formatter('%(name)s - %(levelname)s - %(message)s'))
-    NullHandler = logging.NullHandler()
-
-    CommandPromptLogger = logging.getLogger('commandprompt')
-    CommandPromptLogger.addHandler(ConsoleHandler)
-    CommandPromptLogger.setLevel(logging.DEBUG)
-
+    # ---- Initialize commandprompt logging
+    CommandPromptLogger = sysadmintoolkit.utils.get_logger('commandprompt', dict(config.items('commandprompt')))
     CommandPromptLogger.debug("Started sysadmintoolkit/__main__.py")
 
-    # Load appropriate plugins and instantiate them
-    PluginLogger = logging.getLogger('plugin.%s' % 'commandprompt')
-    PluginLogger.addHandler(ConsoleHandler)
-    PluginLogger.setLevel(logging.DEBUG)
+    # ---- Load plugins and their instance
+    sections = config.sections()
+    sections.sort()
 
-    cmdpromptplugin = commandprompt.CommandPrompt(PluginLogger, config.items('commandprompt'))
+    for section in sections:
+        try:
+            pass
+        except:
+            pass
+
+    PluginLogger = sysadmintoolkit.utils.get_logger('plugin.commandprompt', dict(config.items('commandprompt')))
+
+    if PluginLogger is not None:
+        cmdpromptplugin = commandprompt.CommandPrompt.get_plugin(dict(config.items('commandprompt')), PluginLogger)
+    else:
+        CommandPromptLogger.error('Could not initialize logger for %s' % 'commandprompt')
 
     # Instantiate the main command prompt at the correct mode
-    admincmdprompt = sysadmintoolkit.cmdprompt.CmdPrompt(CommandPromptLogger, mode='admin')
+    admincmdprompt = sysadmintoolkit.cmdprompt.CmdPrompt(CommandPromptLogger, mode='admin', shell_allowed=not args.disable_shell)
 
     def SIGWINCH_handler(signum=None, frame=None):
         (height, width) = sysadmintoolkit.utils.get_terminal_size()
@@ -90,11 +121,13 @@ if __name__ == '__main__':
     # Add each plugin to the commandprompt
     admincmdprompt.add_plugin(cmdpromptplugin)
 
-    print admincmdprompt.command_tree.get_sub_keywords_labels()
-
     # Loop on the main command or directly execute a command
     if len(args.cmd) is 0:
         admincmdprompt.cmdloop()
     else:
+        admincmdprompt.preloop()
+
         for cmd in ' '.join(args.cmd).split(';'):
             admincmdprompt.onecmd(cmd)
+
+        admincmdprompt.postloop()

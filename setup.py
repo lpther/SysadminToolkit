f5522468de648a6d8bedc9aa6051ce637b521abd
Louis-Philippe Theriault <lpther@gmail.com>
2013-04-28 20:14:47 +0000

    * Populated the tests paramters

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distribute_setup import use_setuptools
 use_setuptools()
 
-from setuptools import setup
+from setuptools import *
 
 
 setup(
@@ -9,8 +9,9 @@ setup(
     version='0.1.0',
     author='Louis-Philippe Theriault',
     author_email='lpther@gmail.com',
-    packages=['SysadminToolkit'],
-    url='',
+    packages=find_packages('./sysadmintoolkit'),
+    package_dir={'sysadmintoolkit':'./sysadmintoolkit', 'builtinplugins':'./sysadmintoolkit/builtinplugins'},
+    url='https://github.com/lpther/SysadminToolkit',
     license='See LICENSE.txt',
     description='',
     long_description=open('README.txt').read(),
